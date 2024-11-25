f6ccbd0c1a2b07eafd6397f17f2a3cc3cf2449a2
Enosh Earnest <enoshsolomonn@gmail.com>
2024-11-25 14:47:19 +0000

    removed shared key

--- generate_keys.py
+++
@@ -1,4 +0,0 @@
-# Adjust a key to exactly 16 bytes
-raw_key = b'my_secret_key_too_long!'  # Example of a 19-byte key
-SECRET_KEY = raw_key[:16]  # Truncate to 16 bytes
-print(len(SECRET_KEY))  # Should print 16
